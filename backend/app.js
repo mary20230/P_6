e0c3a70b990ec1b69975301940d9dbd24c18bc58
mary20230 <moracchinimary@gmail.com>
2021-04-05 10:59:40 +0200

    module helmet

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const userRoutes = require('./routes/user')
 const path = require('path');
 const dotenv = require('dotenv')
 dotenv.config();
+const helmet = require('helmet')
 const app = express();
 
 const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ugpgo.mongodb.net/P6?retryWrites=true&w=majority`
@@ -20,7 +21,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-
+app.use(helmet())
 app.use(bodyParser.json())
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', saucesRoutes);
